8b4835e4d209a69af669c0a7cd8063bbbf1ee821
Kelvin Chan <kh.kelvinchan@gmail.com>
2023-02-20 09:44:21 +0000

    URL parsing to work with images online

--- src/png.rs
+++ src/png.rs
@@ -99,7 +99,7 @@ mod tests {
     use crate::chunk::Chunk;
     use crate::chunk_type::ChunkType;
     use std::convert::TryFrom;
-    use std::str::FromStr;
+    // use std::str::FromStr;
 
     fn testing_chunks() -> Vec<Chunk> {
         let mut chunks = Vec::new();
